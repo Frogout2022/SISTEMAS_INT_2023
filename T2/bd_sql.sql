b55e142d89425aa5c5e3c57f390216fd171ef8d7
Milhos <milhos.sihuay@gmail.com>
2023-06-08 08:21:38 -0500

    update sql

--- T2/bd_sql.sql
+++ T2/bd_sql.sql
@@ -3,19 +3,77 @@
 create database if not exists bd_sist_int;
 use bd_sist_int;
 
-CREATE TABLE IF NOT EXISTS Cliente (
-    IDCliente INT AUTO_INCREMENT NOT NULL,
-    Nombre VARCHAR(50) NULL,
-    Direccion VARCHAR(50) NULL,
-    Telefono VARCHAR(13) NULL,
-    Correo VARCHAR(40) NOT NULL,
-    Clave VARCHAR(15) NOT NULL,
-    Distrito VARCHAR(50) NULL,
-    PRIMARY KEY (IDCliente)
-) ;
+CREATE TABLE Empleados (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100),
+  edad INT,
+  salario FLOAT
+);
 
-INSERT INTO CLIENTE (nombre,direccion, correo, clave, distrito) VALUES
-('MILHOS KASSIAN SIHUAY BARZOLA', 'Joaquin Capello 2486','mi@g.com', '123456', 'Lima'),
-('EMERSON GERARDO CAHUANA PEREZ PALMA', 'Direccion3','ge@g.com', '123456', 'Lima');
+CREATE TABLE Clientes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100),
+  direccion VARCHAR(200),
+  email VARCHAR(100)
+);
 
-select * from cliente;
+CREATE TABLE Productos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100),
+  precio FLOAT,
+  stock INT
+);
+
+# --- INSERTAR DATOS
+
+INSERT INTO Empleados (nombre, edad, salario) VALUES
+  ('Juan Pérez', 30, 2500),
+  ('María Gómez', 35, 3000),
+  ('Pedro Rodríguez', 28, 2000);
+
+INSERT INTO Clientes (nombre, direccion, email) VALUES
+  ('Ana López', 'Calle 123', 'ana@email.com'),
+  ('Luis Ramírez', 'Avenida 456', 'luis@email.com');
+
+INSERT INTO Productos (nombre, precio, stock) VALUES
+  ('Producto 1', 10, 50),
+  ('Producto 2', 20, 100),
+  ('Producto 3', 15, 75);
+  
+  
+  # --------- Crear procedimientos almacenados para operaciones CRUD
+  
+  DELIMITER //
+
+CREATE PROCEDURE InsertarEmpleado(
+  IN nombre_param VARCHAR(100),
+  IN edad_param INT,
+  IN salario_param FLOAT
+)
+BEGIN
+  INSERT INTO Empleados (nombre, edad, salario) VALUES (nombre_param, edad_param, salario_param);
+END //
+
+CREATE PROCEDURE ListarEmpleados()
+BEGIN
+  SELECT * FROM Empleados;
+END //
+
+CREATE PROCEDURE ActualizarEmpleado(
+  IN id_param INT,
+  IN nombre_param VARCHAR(100),
+  IN edad_param INT,
+  IN salario_param FLOAT
+)
+BEGIN
+  UPDATE Empleados SET nombre = nombre_param, edad = edad_param, salario = salario_param WHERE id = id_param;
+END //
+
+CREATE PROCEDURE EliminarEmpleado(IN id_param INT)
+BEGIN
+  DELETE FROM Empleados WHERE id = id_param;
+END //
+
+DELIMITER ;
+
+  
